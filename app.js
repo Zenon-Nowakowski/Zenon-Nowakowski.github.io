6c5a59811d9a02df0cfc40d61a42d9c6622a011c
Zenon-Nowakowski <zenon.nowakowski8@gmail.com>
2023-10-01 10:53:08 -0500

    set up firebase hosting

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+
+import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBuITu1J_GaX2A2bPLeg0ABjxyzlNARLGk",
+
+  authDomain: "personal-website-b4081.firebaseapp.com",
+
+  projectId: "personal-website-b4081",
+
+  storageBucket: "personal-website-b4081.appspot.com",
+
+  messagingSenderId: "1043721353841",
+
+  appId: "1:1043721353841:web:5f95f2da18a647e36a79d0",
+
+  measurementId: "G-56NHYYJDVY"
+
+};
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting) {
